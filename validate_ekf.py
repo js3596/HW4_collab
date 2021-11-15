a44a4393229a5a6cf3603238dd98559d943c4740
Jiaxuan Su <suj98@stanford.edu>
2021-11-15 04:30:50 -0800

    Done P2

--- validate_ekf.py
+++ validate_ekf.py
@@ -317,9 +317,9 @@ if __name__ == '__main__':
     #validate_ekf_localization()
 
     ## Subcomponent validation
-    #validate_localization_transition_model()
-    #validate_localization_compute_predicted_measurements()
-    #validate_localization_compute_innovations()
+    validate_localization_transition_model()
+    validate_localization_compute_predicted_measurements()
+    validate_localization_compute_innovations()
 
     ### PROBLEM 2
 
